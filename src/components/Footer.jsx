2389d7244e7dfb9e0a6f7fe47156b0b993313eba
kevin shah <126804721+KEVINs121998@users.noreply.github.com>
2023-06-09 23:58:40 +0530

    removed 0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,17 @@ import {FaFacebookSquare, FaInstagram ,FaWhatsapp } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>
+    <footer className='fixed bottom-0 right-0 bg-red-700'>
+    <div className='h-20 '>
         <div className='flex flex-row items-center float-right'>
             Follow US: <div className='px-4 flex flex-row justify-around '>
                 <a className='' href='https://www.facebook.com/DHUMALMOTORZ' target='_blank'><FaFacebookSquare size={30}/></a>
-                <a href='http://wa.me/+0917303678465' target='_blank'><FaWhatsapp size={30}/></a>
+                <a href='http://wa.me/+917303678465' target='_blank'><FaWhatsapp size={30}/></a>
                 <a href='https://instagram.com/dhumalmotors?igshid=MmJiY2I4NDBkZg==' target='_blank'><FaInstagram size={30}/></a>
             </div>
         </div>
     </div>
+    </footer>
   )
 }
 
