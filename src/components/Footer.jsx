0ab901239c89ca39aaa466806783c3262dbfaa44
kevin shah <126804721+KEVINs121998@users.noreply.github.com>
2023-06-09 23:39:51 +0530

    size of social

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <div>
         <div className='flex flex-row items-center float-right'>
             Follow US: <div className='px-4 flex flex-row justify-around '>
-                <a href='https://www.facebook.com/DHUMALMOTORZ' target='_blank'><FaFacebookSquare/></a>
-                <a href='http://wa.me/+0917303678465' target='_blank'><FaWhatsapp/></a>
-                <a href='https://instagram.com/dhumalmotors?igshid=MmJiY2I4NDBkZg==' target='_blank'><FaInstagram/></a>
+                <a className='' href='https://www.facebook.com/DHUMALMOTORZ' target='_blank'><FaFacebookSquare size={30}/></a>
+                <a href='http://wa.me/+0917303678465' target='_blank'><FaWhatsapp size={30}/></a>
+                <a href='https://instagram.com/dhumalmotors?igshid=MmJiY2I4NDBkZg==' target='_blank'><FaInstagram size={30}/></a>
             </div>
         </div>
     </div>
